4e63ce7fb58856ef81e576f77c36e5e7c591dade
Rohit Verma <1308rohitv@gmaiil.com>
2026-04-21 01:14:18 +0530

    chore:add .gitignore and remove _site from tracking

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "1. Two Sum",
-        "excerpt":"Problem Statement Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice. You can return the answer...","categories": [],
-        "tags": [],
-        "url": "/Leetcode_POTD/potd/2026-04-15-two-sum/",
-        "teaser": null
-      }]
